3c6544d5c5d8bef42eac71381bfb42c19f3887fe
Yuri Ramos <yuri.pramos@gmail.com>
2017-07-29 13:21:46 -0300

    updating app.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,40 +1,37 @@
-import "./App.css";
 import React, { Component } from 'react';
 import ReactDOM from "react-dom";
+import "./App.css";
 import Footer from "../components/footer/Footer.js";
 import ComponentA from "../components/component_a/ComponentA.js";
 import ComponentB from "../components/component_b/ComponentB.js";
 
 
-
-
-
 class App extends React.Component {
-    constructor(props){
-    	super(props);
-	    this.state = {
-	    	name:"Yuri",
-	    	lastname:"Ramos"
-	    };
-    }
-	render(){
-		return(
-			<div>
-				<div className="container">
-					<div className="row">
-						<div className="col-6 text-center">
-							<ComponentA name={this.state.name} />
-						</div>
-						<div className="col-6 text-center">
-							<ComponentB lastname={this.state.lastname} />
-						</div>
-					</div>	
-				</div>
-				<Footer />
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Yuri',
+      lastname: 'Ramos',
+    };
+  }
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-6 text-center">
+              <ComponentA name={this.state.name} />
+            </div>
+            <div className="col-6 text-center">
+              <ComponentB lastname={this.state.lastname} />
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 
-ReactDOM.render(<App/>, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
